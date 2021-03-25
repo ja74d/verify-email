e7d5f0fcde50fb56e7dc379b2ed9a7230a2e5132
javad pournosrat <pournosrat.javad@gmail.com>
2021-03-25 21:44:05 +0430

    verify-email.py

---
+++ verify-email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.9
+
+from verify_email import verify_email
+
+email = input(str('enter email:'))
+
+result = verify_email(email)
+
+if result == True:
+	print(f'<<{email}>> is exist!')
+else:
+	print(f'<<{email}>> is not exist!!!')
